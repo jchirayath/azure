1accb8b621c106d51eb2887b0fbb240f70b569ba
jchirayath <jacobjc@gmail.com>
2018-04-14 15:31:11 -0700

    Added Updates

--- VMCreate/install_script.sh
+++ VMCreate/install_script.sh
@@ -9,9 +9,9 @@ apt-get -y install apache2
 apt-get -y install php
 apt-get -y install php-mysql
 apt-get -y install mysql
-apt-get -y install nginx
-apt-get -y install privoxy
-apt-get -y install sssd adcli realmd sssd-krb5-common krb5-user samba-common
+#apt-get -y install nginx
+#apt-get -y install privoxy
+apt-get -y install sssd adcli realmd samba-common
 #apt-get -y install sssd realmd adcli krb5-workstation samba-common
 apt-get -y install expect unzip nmap nfs-utils rsync screen diffutils lsof 
 apt-get -y install tcpdump telnet nc traceroute wget perl curl
@@ -48,11 +48,10 @@ cp index.html /var/www/html/index.html
 # Install and configure Mail
 debconf-set-selections <<< "postfix postfix/mailname string $HOSTNAME"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
-apt-get install -y mailutils
+apt-get -y install mailutils
 
 # Install and run Lynis Security Scanner
 git clone https://github.com/CISOfy/lynis
-cd lynis; ./lynis audit system
-cat /var/log/lynis.log | mail -s "PEN test for Host $HOSTNAME" jacobjc@hotmail.com
+cd lynis; ./lynis audit system -Q --no-colors | mail -s "PEN test for Host $HOSTNAME" jacobjc@hotmail.com
 
 # END of SCRIPT
