8beec470745c977566b7e162d67dff35d525f434
jchirayath <jacobjc@gmail.com>
2018-04-14 13:45:34 -0700

    Added Updates

---
+++ VMCreate/CreateWin2016.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git commit -a -m "Added Updates"
+git push
+az account show
+az account set --subscription JacobAzure
+az group create --name RG-JacobAzureWindows --location "West US"
+az group deployment create \
+    --name WindowsVM \
+    --resource-group RG-JacobAzureWindows \
+    --template-file azuredeployWin2016.json \
+    --parameters @azuredeployWin2016.parameters.json
