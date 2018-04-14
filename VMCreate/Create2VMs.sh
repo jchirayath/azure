338ba9b826be87cfca879b9348dfda257e5a0a51
jchirayath <jacobjc@gmail.com>
2018-04-14 13:10:49 -0700

    Added Updates

--- VMCreate/Create2VMs.sh
+++ VMCreate/Create2VMs.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+git commit -a -m "Added Updates"
+git push
 az account show
 az account set --subscription JacobAzure
 az group create --name RG-JacobAzure2VMs --location "West US"
 az group deployment create \
     --name LinuxVM \
-    --resource-group RG-JacobAzureVMs \
+    --resource-group RG-JacobAzure2VMs \
     --template-file azuredeploy2.json \
     --parameters @azuredeploy2.parameters.json
