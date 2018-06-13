5405f41e90dbe4465208afd9cef039e7182b90b8
jchirayath <jacobjc@gmail.com>
2018-06-12 22:09:43 -0700

    test

---
+++ VMCreate/CreateRG-IngniteTest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git commit -a -m "Added Updates"
+git push
+az account show
+az account set --subscription "Master Lighthouse MS Alliance Subscription(Converted to EA)"
+az group create --name RG-IgniteTest --location "West US"
+az group deployment create \
+    --name LinuxVM \
+    --resource-group RG-IgniteTest \
+    --template-file azuredeployjc.json \
+    --parameters @azuredeploy.parameters.json
