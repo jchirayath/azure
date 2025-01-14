34ca95993880b51b7cb6ffdf66a0313517e7c60d
Jacob <jacobjc@gmail.com>
2025-01-13 20:16:53 -0800

    Creating AXA

--- AxaCreate/CreateVM.sh
+++ AxaCreate/CreateVM.sh
@@ -4,9 +4,9 @@ git push
 az account show
 az account set --subscription JacobAzure
 
-# Create azure resource group called DefaultResourceGroup-WUS if it does not exist
-if ! az group show --name DefaultResourceGroup-WUS &>/dev/null; then
-    az group create --name DefaultResourceGroup-WUS --location "West US"
+# Create azure resource group called rg-VMs-westus3 if it does not exist
+if ! az group show --name rg-VMs-westus3 &>/dev/null; then
+    az group create --name rg-VMs-westus3 --location "West US 3"
 fi
 
 # Create azure VM if it does not exist
@@ -22,30 +22,35 @@ fi
 az vm create \
     --resource-group DefaultResourceGroup-WUS \
     --name axa \
-    --image UbuntuLatest \
-    --ssh-key-value /.ssh/azure_id.pub \
-    --admin-username azureuser
+    --image Ubuntu2204 \
+    --size Standard_D2s_v3 \
+    --assign-identity \
+    --admin-username azureuser \
+    --enable-agent true \
+    --enable-auto-update true \
+    --public-ip-address ${DNS_HOSTNAME}-ip \
+    --ssh-key-value /path/to/your/azure_id.pub
 
-# Add a custom script extension to the VM to run an install script
-az vm extension set \
-    --resource-group DefaultResourceGroup-WUS \
-    --vm-name axa \
-    --name customScript \
-    --publisher Microsoft.Azure.Extensions \
-    --settings '{"fileUris": ["https://raw.githubusercontent.com/jchirayath/azure/master/azure/AxaCreate/install_script.sh"], "commandToExecute": "./install_script.sh"}'
+# # Add a custom script extension to the VM to run an install script
+# az vm extension set \
+#     --resource-group DefaultResourceGroup-WUS \
+#     --vm-name axa \
+#     --name customScript \
+#     --publisher Microsoft.Azure.Extensions \
+#     --settings '{"fileUris": ["https://raw.githubusercontent.com/jchirayath/azure/master/azure/AxaCreate/install_script.sh"], "commandToExecute": "./install_script.sh"}'
 
-# Add Azure CLI extension to the VM
-az vm extension set \
-    --resource-group DefaultResourceGroup-WUS \
-    --vm-name axa \
-    --name AzureCli \
-    --publisher Microsoft.Azure.Extensions \
-    --settings '{}'
+# # Add Azure CLI extension to the VM
+# az vm extension set \
+#     --resource-group DefaultResourceGroup-WUS \
+#     --vm-name axa \
+#     --name AzureCli \
+#     --publisher Microsoft.Azure.Extensions \
+#     --settings '{}'
 
-# Add Azure SSH extension to the VM
-az vm extension set \
-    --resource-group DefaultResourceGroup-WUS \
-    --vm-name axa \
-    --name VMAccessForLinux \
-    --publisher Microsoft.OSTCExtensions \
-    --settings '{}'
+# # Add Azure SSH extension to the VM
+# az vm extension set \
+#     --resource-group DefaultResourceGroup-WUS \
+#     --vm-name axa \
+#     --name VMAccessForLinux \
+#     --publisher Microsoft.OSTCExtensions \
+#     --settings '{}'
