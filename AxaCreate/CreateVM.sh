5c6c6c6106d37fa601366921422827b22fb94bac
Jacob Chirayath <jacobc@Jacobs-Mac-mini.local>
2025-01-14 21:44:30 -0800

    Creating AXA

--- AxaCreate/CreateVM.sh
+++ AxaCreate/CreateVM.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+# Exit if any command fails
+set -e
+
 # Commit Code to GitHub
-# git commit -a -m "Creating AXA"
-# git push
+git commit -a -m "Creating AXA"
+git push
 
 # Check Account and Set Subscription
 az account show
 az account set --subscription JacobAzure
-# Exit if any command fails
-set -e
 
 # Set VM variables
 VM_RESOURCE_GROUP="rg-VMs"
@@ -22,58 +23,64 @@ VM_INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/jchirayath/azure/refs/h
 # KEY Vault Admins
 KEY_VAULT_ADMINS='AAD DC Administrators'
 
-# # Create azure resource group called rg-VMs-westus3 if it does not exist
-# az group create --name $VM_RESOURCE_GROUP-$VM_REGION --location $VM_REGION
-# # Exit if any command fails
-# set -e
+# Get password from environment variable for Guacamole DB
+if [ -z "$MY_PASSWORD" ]; then
+    echo "Error: MY_PASSWORD environment variable is not set."
+    read -sp 'Enter password for Guacamole DB: ' MY_PASSWORD
+    echo
+    exit 1
+fi
 
-# # Create azure DNS Nam
-# az network public-ip create --resource-group $VM_RESOURCE_GROUP-$VM_REGION --name ${VM_HOSTNAME}-ip --dns-name ${VM_HOSTNAME}
-# # Exit if any command fails
-# set -e
+# Get password from environment variable for MYSQL_ADMIN_PASSWORD
+if [ -z "$MYSQL_ADMIN_PASSWORD" ]; then
+    echo "Error: MYSQL_ADMIN_PASSWORD environment variable is not set."
+    read -sp 'Enter MySQL admin password: ' MYSQL_ADMIN_PASSWORD
+    echo
+    exit 1
+fi
 
-# # Create a network security group
-# az network nsg create --resource-group $VM_RESOURCE_GROUP-$VM_REGION --name ${VM_HOSTNAME}-nsg
-# # Exit if any command fails
-# set -e
+# Create azure resource group called rg-VMs-westus3 if it does not exist
+az group create --name $VM_RESOURCE_GROUP-$VM_REGION --location $VM_REGION
 
-# # Create network security group rules
-# az network nsg rule create --resource-group $VM_RESOURCE_GROUP-$VM_REGION --nsg-name ${VM_HOSTNAME}-nsg --name AllowSSH --protocol tcp --priority 1000 --destination-port-range 22 --access allow
-# az network nsg rule create --resource-group $VM_RESOURCE_GROUP-$VM_REGION --nsg-name ${VM_HOSTNAME}-nsg --name AllowHTTP --protocol tcp --priority 1010 --destination-port-range 80 --access allow
-# az network nsg rule create --resource-group $VM_RESOURCE_GROUP-$VM_REGION --nsg-name ${VM_HOSTNAME}-nsg --name AllowHTTPS --protocol tcp --priority 1020 --destination-port-range 443 --access allow
-# az network nsg rule create --resource-group $VM_RESOURCE_GROUP-$VM_REGION --nsg-name ${VM_HOSTNAME}-nsg --name Allow8080 --protocol tcp --priority 1030 --destination-port-range 8080 --access allow
-# # Exit if any command fails
-# set -e
+# Create azure DNS Nam
+az network public-ip create --resource-group $VM_RESOURCE_GROUP-$VM_REGION --name ${VM_HOSTNAME}-ip --dns-name ${VM_HOSTNAME}
 
-# # Create the VM
-# az vm create \
-#     --resource-group $VM_RESOURCE_GROUP-$VM_REGION \
-#     --name $VM_HOSTNAME \
-#     --image $VM_OS \
-#     --size $VM_SIZE \
-#     --assign-identity \
-#     --admin-username azureuser \
-#     --enable-agent true \
-#     --enable-auto-update true \
-#     --enable-secure-boot true \
-#     --enable-vtpm true \
-#     --public-ip-address ${VM_HOSTNAME}-ip \
-#     --nsg ${VM_HOSTNAME}-nsg \
-#     --os-disk-delete-option Delete \
-#     --os-disk-name ${VM_HOSTNAME}-osdisk \
-#     --os-disk-size-gb $VM_DISK_SIZE \
-#     --vnet-name ${VM_HOSTNAME}-vnet \
-#     --ssh-key-value ~/.ssh/$VM_AZURE_KEY.pub
-# # Exit if any command fails
-# set -e
+# Create a network security group
+az network nsg create --resource-group $VM_RESOURCE_GROUP-$VM_REGION --name ${VM_HOSTNAME}-nsg
+
+# Create network security group rules
+az network nsg rule create --resource-group $VM_RESOURCE_GROUP-$VM_REGION --nsg-name ${VM_HOSTNAME}-nsg --name AllowSSH --protocol tcp --priority 1000 --destination-port-range 22 --access allow
+az network nsg rule create --resource-group $VM_RESOURCE_GROUP-$VM_REGION --nsg-name ${VM_HOSTNAME}-nsg --name AllowHTTP --protocol tcp --priority 1010 --destination-port-range 80 --access allow
+az network nsg rule create --resource-group $VM_RESOURCE_GROUP-$VM_REGION --nsg-name ${VM_HOSTNAME}-nsg --name AllowHTTPS --protocol tcp --priority 1020 --destination-port-range 443 --access allow
+az network nsg rule create --resource-group $VM_RESOURCE_GROUP-$VM_REGION --nsg-name ${VM_HOSTNAME}-nsg --name Allow8080 --protocol tcp --priority 1030 --destination-port-range 8080 --access allow
+
+# Create the VM
+az vm create \
+    --resource-group $VM_RESOURCE_GROUP-$VM_REGION \
+    --name $VM_HOSTNAME \
+    --image $VM_OS \
+    --size $VM_SIZE \
+    --assign-identity \
+    --admin-username azureuser \
+    --enable-agent true \
+    --enable-auto-update true \
+    --enable-secure-boot true \
+    --enable-vtpm true \
+    --public-ip-address ${VM_HOSTNAME}-ip \
+    --nsg ${VM_HOSTNAME}-nsg \
+    --os-disk-delete-option Delete \
+    --os-disk-name ${VM_HOSTNAME}-osdisk \
+    --os-disk-size-gb $VM_DISK_SIZE \
+    --vnet-name ${VM_HOSTNAME}-vnet \
+    --ssh-key-value ~/.ssh/$VM_AZURE_KEY.pub
 
 # Install VM extension AADLoginForLinux
-# az vm extension set \
-#     --resource-group $VM_RESOURCE_GROUP-$VM_REGION \
-#     --vm-name $VM_HOSTNAME \
-#     --name AADSSHLoginForLinux \
-#     --publisher Microsoft.Azure.ActiveDirectory \
-#     --settings '{}'
+az vm extension set \
+    --resource-group $VM_RESOURCE_GROUP-$VM_REGION \
+    --vm-name $VM_HOSTNAME \
+    --name AADSSHLoginForLinux \
+    --publisher Microsoft.Azure.ActiveDirectory \
+    --settings '{}'
 
 # Create a Key Vault
 # Check if Key Vault exists, create if it does not
@@ -86,31 +93,28 @@ else
     echo "Key Vault $VAULT_NAME already exists."
 fi
 
-# # Allow Azure admins to add secrets
-# ADMIN_GROUP_ID=$(az ad group show --group "$KEY_VAULT_ADMINS" --query id -o tsv)
-# az role assignment create --role "Key Vault Administrator" --assignee $ADMIN_GROUP_ID --scope /subscriptions/$(az account show --query id -o tsv)/resourceGroups/$VM_RESOURCE_GROUP-$VM_REGION/providers/Microsoft.KeyVault/vaults/$VAULT_NAME
+# Allow Azure admins to add secrets
+ADMIN_GROUP_ID=$(az ad group show --group "$KEY_VAULT_ADMINS" --query id -o tsv)
+az role assignment create --role "Key Vault Administrator" --assignee $ADMIN_GROUP_ID --scope /subscriptions/$(az account show --query id -o tsv)/resourceGroups/$VM_RESOURCE_GROUP-$VM_REGION/providers/Microsoft.KeyVault/vaults/$VAULT_NAME
 
-# # wait for the role assignment to propagate
-# sleep 15
+# wait for the role assignment to propagate
+sleep 15
 
 # # Create Secret in Azure Key Vault for Guacamole
-# myMyuSQLHOST="den1.mysql6.gear.host"
-# myUser="guacamoledb"
-# # Get password from environment variable
-# if [ -z "$MY_PASSWORD" ]; then
-#     echo "Error: MY_PASSWORD environment variable is not set."
-#     exit 1
-# fi
-# myPassword=$MY_PASSWORD
-# az keyvault secret set --vault-name $VAULT_NAME --name "guacamoledbhost" --value $myMyuSQLHOST
-# az keyvault secret set --vault-name $VAULT_NAME --name "guacamoledbuser" --value $myUser
-# az keyvault secret set --vault-name $VAULT_NAME --name "guacamoledbpass" --value $myPassword
+myMyuSQLHOST="den1.mysql6.gear.host"
+myUser="guacamoledb"
+myPassword=$MY_PASSWORD
+az keyvault secret set --vault-name $VAULT_NAME --name "guacamoledbhost" --value $myMyuSQLHOST
+az keyvault secret set --vault-name $VAULT_NAME --name "guacamoledbuser" --value $myUser
+az keyvault secret set --vault-name $VAULT_NAME --name "guacamoledbpass" --value $myPassword
+
+# Add MySQL admin password to Key Vault
+az keyvault secret set --vault-name $VAULT_NAME --name "mysql-admin-password" --value $MYSQL_ADMIN_PASSWORD
 
 # Enable the VM to query Key Vault secrets
 az vm identity assign --resource-group $VM_RESOURCE_GROUP-$VM_REGION --name $VM_HOSTNAME
 VM_IDENTITY=$(az vm show --resource-group $VM_RESOURCE_GROUP-$VM_REGION --name $VM_HOSTNAME --query identity.principalId -o tsv)
 az role assignment create --role "Key Vault Reader" --assignee $VM_IDENTITY --scope /subscriptions/$(az account show --query id -o tsv)/resourceGroups/$VM_RESOURCE_GROUP-$VM_REGION/providers/Microsoft.KeyVault/vaults/$VAULT_NAME
-
 
 # # Add a custom script extension to the VM to run an install script
 # az vm extension set \
--- AxaCreate/install_script.sh
+++ AxaCreate/install_script.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
+# Exit if any command fails
+set -e
+
 # Update APT repository
 apt-get -y update
+
 # Install Software
-apt-get -y install docker docker.io
+apt-get -y install docker.io
 apt-get -y install apache2
 apt-get -y install php
 apt-get -y install php-mysql
@@ -25,52 +30,51 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO 
 apt-get update
 apt-get install azure-cli
 
+# Define Web Hostname
+myHOST="axa.$VM_REGION.cloudapp.azure.com"
+
 # Assumption is that DBs are hosted on GearHost.com
 # That way I dont need Pay for a database service in Azure seperately.
-# Get hostname from Azure Key Vault
 VM_REGION="westus3"
 VAULT_NAME="kv-aspl-$VM_REGION"
 EMAIL_USER='jacobc@aspl.net'
 
+# Get hostname/username/password from Azure Key Vault
+myMySQLHOST=$(az keyvault secret show --vault-name $VAULT_NAME --name "guacamoledbhost" --query value -o tsv)
+myMySQLUSER=$(az keyvault secret show --vault-name $VAULT_NAME --name "guacamoledbuser" --query value -o tsv)
+myMySQLPASS=$(az keyvault secret show --vault-name $VAULT_NAME --name "guacamoledbpass" --query value -o tsv)
 
-myMyuSQLHOST=$(az keyvault secret show --vault-name $VAULT_NAME --name "guacamoledbhost" --query value -o tsv)
-myUser=$(az keyvault secret show --vault-name $VAULT_NAME --name "guacamoledbuser" --query value -o tsv)
-myPassword=$(az keyvault secret show --vault-name $VAULT_NAME --name "guacamoledbpass" --query value -o tsv)
+# # Guacamole Install
+# docker run --name some-guacd -d guacamole/guacd
+# # Install Start Docker GUACAMOLE
+# docker run --name some-guacamole --link some-guacd:guacd \
+#     -e MYSQL_HOSTNAME=$myMySQLHOST \
+#     -e MYSQL_DATABASE=guacamoledb  \
+#     -e MYSQL_USER=$myMySQLUSER    \
+#     -e MYSQL_PASSWORD=$myMySQLPASS \
+#     -d -p 8080:8080 guacamole/guacamole
 
-# Define Web Hostname
-myHOST=“axa.$VM_REGION.cloudapp.azure.com”
+# # Update the Guacamole Tomcat Configuration
+# mysql --host=$myMySQLHOST --user=$myMySQLUSER --password=$myMySQLPASS -e "intidb.sql"
 
-# Guacamole Install
-docker run --name some-guacd -d guacamole/guacd
-# Install Start Docker GUACAMOLE
-docker run --name some-guacamole --link some-guacd:guacd \
-    -e MYSQL_HOSTNAME=$myMyuSQLHOST \
-    -e MYSQL_DATABASE=guacamoledb  \
-    -e MYSQL_USER=$myUser    \
-    -e MYSQL_PASSWORD=$myPassword \
-    -d -p 8080:8080 guacamole/guacamole
+# # GUACD Install
+# docker run --name some-guacd -d guacamole/guacd
+# docker run --name some-guacamole --link some-guacd:guacd \
+#     -e MYSQL_HOSTNAME=$myMySQLHOST \
+#     -e MYSQL_DATABASE=guacamoledb  \
+#     -e MYSQL_USER=$myMySQLUSER    \
+#     -e MYSQL_PASSWORD=$myMySQLPASS \
+#     -d -p 8080:8080 guacamole/guacamole
 
-# Update the Guacamole Tomcat Configuration
-mysql --host=$myMyuSQLHOST --user=$myUser --password=$myPassword -e "intidb.sql"
+# # Fix  Tomcat Configuration for guacamole
+# # Get username and password from Azure Key Vault
+# guacAdminUser=$(az keyvault secret show --vault-name $VAULT_NAME --name "guacadminuser" --query value -o tsv)
+# guacAdminPassword=$(az keyvault secret show --vault-name $VAULT_NAME --name "guacadminpass" --query value -o tsv)
+# docker exec -it some-guacamole bash -c "sed -i 's/<\/tomcat-users>/  <role rolename=\"guacamole-admin\"\/>\n  <user username=\"tomcat\" password=\"$guacAdminPassword\" roles=\"guacamole-admin\"\/>\n<\/tomcat-users>/' /usr/local/tomcat/conf/tomcat-users.xml"
 
-# GUACD Install
-docker run --name some-guacd -d guacamole/guacd
-docker run --name some-guacamole --link some-guacd:guacd \
-    -e MYSQL_HOSTNAME=$myMyuSQLHOST \
-    -e MYSQL_DATABASE=guacamoledb  \
-    -e MYSQL_USER=$myUser    \
-    -e MYSQL_PASSWORD=$myPassword \
-    -d -p 8080:8080 guacamole/guacamole
-
-# Fix  Tomcat Configuration for guacamole
-# Get username and password from Azure Key Vault
-guacAdminUser=$(az keyvault secret show --vault-name $VAULT_NAME --name "guacadminuser" --query value -o tsv)
-guacAdminPassword=$(az keyvault secret show --vault-name $VAULT_NAME --name "guacadminpass" --query value -o tsv)
-docker exec -it some-guacamole bash -c "sed -i 's/<\/tomcat-users>/  <role rolename=\"guacamole-admin\"\/>\n  <user username=\"tomcat\" password=\"$guacAdminPassword\" roles=\"guacamole-admin\"\/>\n<\/tomcat-users>/' /usr/local/tomcat/conf/tomcat-users.xml"
-
-# Restart Docker Guacamole and Guacd
-docker update  --restart unless-stopped some-guacamole
-docker update  --restart unless-stopped some-guacd
+# # Restart Docker Guacamole and Guacd
+# docker update  --restart unless-stopped some-guacamole
+# docker update  --restart unless-stopped some-guacd
 
 # Get Updated Index.htm from GitHub
 wget https://raw.githubusercontent.com/jchirayath/aws/master/s3/config/index.html
@@ -106,6 +110,17 @@ apt install python3-certbot-apache
 apt install python3-certbot-nginx
 certbot --apache -d $myHOST --non-interactive --agree-tos -m $EMAIL_USER
 certbot --nginx -d $myHOST --non-interactive --agree-tos -m $EMAIL_USER
+
+# Install mysqldaemon
+apt-get -y install mysql-server
+systemctl start mysql
+systemctl enable mysql
+
+# Enable default access policies for mysqldaemon
+MYSQL_ADMIN_PASSWORD=$(az keyvault secret show --vault-name $VAULT_NAME --name "mysqladminpass" --query value -o tsv)
+mysql -e "CREATE USER 'admin'@'%' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;"
+mysql -e "FLUSH PRIVILEGES;"
 
 # install cloudpanel
 curl -sSL https://installer.cloudpanel.io/ce/v2/install.sh | sudo bash
