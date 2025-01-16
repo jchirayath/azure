3ebbd2b3c6b4f3b5ad5ba5ac0b8e93e2b742df09
Jacob <jacobjc@gmail.com>
2025-01-15 19:24:29 -0800

    Update install_script_template.sh

--- AxaCreate/install_script_template.sh
+++ AxaCreate/install_script_template.sh
@@ -17,6 +17,24 @@ myVAULT_NAME="<VAULT_NAME>"
 echo "Setting hostname to $myVM_HOST"
 sudo hostnamectl set-hostname $myVM_HOST
 
+# Update resolv.conf to include FQDN
+echo "Setting Fully Qualified Domain Name (FQDN) to $myVM_HOST.$myVM_REGION.cloudapp.azure.com"
+echo "$myVM_HOST.$myVM_REGION.cloudapp.azure.com" | sudo tee /etc/hostname
+echo "127.0.1.1 $myVM_HOST.$myVM_REGION.cloudapp.azure.com $myVM_HOST" | sudo tee -a /etc/hosts
+
+# Update resolv.conf to include FQDN
+echo "Updating resolv.conf to include FQDN"
+sudo sed -i "s/search.*/search $myVM_HOST.$myVM_REGION.cloudapp.azure.com/g" /etc/resolv.conf
+
+## Set the timezone to California
+echo "Setting the timezone to California"
+sudo timedatectl set-timezone America/Los_Angeles
+
+## Set the locale to en_US.UTF-8
+echo "Setting the locale to en_US.UTF-8"
+sudo locale-gen en_US.UTF-8
+sudo update-locale LANG=en_US.UTF-8
+
 ## Update the package list and upgrade all installed packages
 echo "Updating package list and upgrading installed packages"
 sudo apt-get update -y
