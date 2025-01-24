2d796e790e607191eaae92b28c0b143ac060cbcc
Jacob <jacobjc@gmail.com>
2025-01-24 01:01:37 -0800

    Update

--- CreateAxa/scripts/Mail_Setup.sh
+++ CreateAxa/scripts/Mail_Setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-# Function to set up mail
-function setup_mail() {
-    echo "Setting up mail..."
-    # Add your mail setup commands here
-}
-setup_mail
+
+# Setup and Configure Mail
+# Install postfix
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y postfix
+
+# configure postfix non interactive
+sudo debconf-set-selections <<< "postfix postfix/mailname string $HOSTNAME"
+sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+
+# Restart postfix to apply changes
+sudo systemctl restart postfix
