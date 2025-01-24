7643abe043313e53a0e53cd93f9fe2120c4c1ab4
Jacob <jacobjc@gmail.com>
2025-01-23 22:44:10 -0800

    Update

--- CreateAxa/scripts/Firewall_Install.sh
+++ CreateAxa/scripts/Firewall_Install.sh
@@ -1,8 +1,42 @@
 #!/bin/bash
+
 # Function to set up firewall
 function setup_firewall() {
+
+    # Register application with UFW
     echo "Setting up firewall..."
     sudo ufw allow OpenSSH
+    sudo ufw allow "Apache Full"
+    # Allow the following ports
+    sudo ufw allow 3306/tcp  # MySQL
+    sudo ufw allow 25/tcp    # Postfix
+    sudo ufw allow 587/tcp   # Postfix
+    sudo ufw allow 465/tcp   # Postfix
+    sudo ufw allow 143/tcp   # IMAP
+    sudo ufw allow 993/tcp   # IMAPS
+    sudo ufw allow 80/tcp    # HTTP
+    sudo ufw allow 443/tcp   # HTTPS
+    sudo ufw allow 8080/tcp  # Tomcat
+    sudo ufw allow 8118/tcp  # Privoxy
+    sudo ufw allow 8080/tcp  # Tomcat
+    sudo ufw allow 8118/tcp  # Privoxy
+
+    # Enable UFW
+    echo "Enabling firewall..."
     sudo ufw enable
 }
+
+# Call the function
+echo "## Setting up firewall"
 setup_firewall
+# Check for errors
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to set up firewall"
+    exit 1
+else
+    echo "## Setting up firewall - Done"
+fi
+
+# Exit the script
+echo "## Firewall setup complete"
+exit 0
