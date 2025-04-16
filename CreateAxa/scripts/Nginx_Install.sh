affcb0504e9712eab1dd84367477f5cf3a3c0ff1
Jacob <jacobjc@gmail.com>
2025-04-15 20:43:06 -0700

    updates

--- CreateAxa/scripts/Nginx_Install.sh
+++ CreateAxa/scripts/Nginx_Install.sh
@@ -84,6 +84,26 @@ server {
         proxy_set_header Connection \$http_connection;
         access_log off;
     }
+
+    location /webmin/ {
+        proxy_pass http://localhost:10000/;
+        proxy_buffering off;
+        proxy_http_version 1.1;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection \$http_connection;
+        access_log off;
+    }
+
+    location /privoxy/ {
+        proxy_pass http://localhost:8118/;
+        proxy_buffering off;
+        proxy_http_version 1.1;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection \$http_connection;
+        access_log off;
+    }
 }
 EOF
     then
--- CreateAxa/scripts/Take_Backup.sh
+++ CreateAxa/scripts/Take_Backup.sh
@@ -3,20 +3,28 @@
 # Take a local OS ubuntu snapshot
 echo "## Installing timeshift and taking initial setup snapshot"
 
-# Update package list and install timeshift
+# Update package list to ensure we have the latest package information
+echo "Updating package list..."
 sudo apt-get update
+
+# Install the timeshift package, which is used for creating system snapshots
+echo "Installing timeshift package..."
 sudo apt-get install -y timeshift
 
-# Create an initial setup snapshot with timeshift
+# Create an initial setup snapshot with timeshift and tag it as a daily snapshot
+echo "Creating an initial setup snapshot with timeshift..."
 sudo timeshift --create --comments "Initial setup snapshot" --tags D
 
-# Check if the snapshot was created successfully
+# Check the exit status of the previous command to determine if the snapshot was created successfully
 if [ $? -eq 0 ]; then
+    # Print a success message if the snapshot was created without errors
     echo "Snapshot created successfully"
 else
+    # Print an error message and exit the script with a non-zero status if the snapshot creation failed
     echo "Failed to create snapshot"
     exit 1
 fi
 
-# Exit the script
+# Exit the script with a success status
+echo "Exiting script successfully."
 exit 0
